3e807f0f630cb8d5b9ac4dfd063cac19b002fbfb
Vaibhav <vaibhavkumarjak@gmail.com>
2026-06-20 15:00:33 +0530

    Add unique auto generated slug feature

--- store/models.py
+++ store/models.py
@@ -1,12 +1,22 @@
 from django.db import models
-
+from django.utils.text import slugify
 # Create your models here.
 class Product(models.Model):
     name = models.CharField(max_length=100)
     price = models.FloatField()
     desc = models.TextField()
     image = models.ImageField(upload_to="images/")
-    slug = models.SlugField(max_length=100)
+    slug = models.SlugField(unique=True,blank=True)
     stock = models.IntegerField()
     active = models.BooleanField()
     
+    def save(self,*args,**kwargs):
+        if not self.slug:
+            base_slug = slugify(self.name)
+            slug = base_slug
+            counter = 1
+            while Product.objects.filter(slug=slug).exists():
+                slug = f"{base_slug}-{counter}"
+                counter += 1
+            self.slug = slug
+        super().save(*args,**kwargs)
