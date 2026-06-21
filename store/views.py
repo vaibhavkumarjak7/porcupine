e58a6539fa20bdc534109edf2acd5507fa2f604f
Vaibhav <vaibhavkumarjak@gmail.com>
2026-06-21 17:48:01 +0530

    Index page initial setup

--- store/views.py
+++ store/views.py
@@ -1,3 +1,6 @@
 from django.shortcuts import render
-
+from .models import Product
 # Create your views here.
+def index(request):
+    products = Product.objects.all()
+    return render(request,'store/index.html',{'products':products})
