55fa41c025e57318c5a557668832da745a6d8f22
Vaibhav <vaibhavkumarjak@gmail.com>
2026-06-24 08:20:17 +0530

    Design the detail page

--- store/views.py
+++ store/views.py
@@ -4,3 +4,7 @@ from .models import Product
 def index(request):
     products = Product.objects.all()
     return render(request,'store/index.html',{'products':products})
+
+def detail(request,slug):
+    product = Product.objects.get(slug=slug)
+    return render(request,'store/detail.html',{'product': product})
